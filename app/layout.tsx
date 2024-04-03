c17f121016ea189f97bec0304970892200ebc3dd
EthanL06 <ethanlanting@gmail.com>
2024-04-03 01:52:50 -0500

    Fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import NavigationMenu from "@/components/navigation-menu";
 
 import { Poppins as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
